3aa15349aed2a48b3eb77ed3a33ccc9c5167537d
FedericoMarinC02 <federico.marin.cardona@gmail.com>
2025-05-15 17:30:13 -0500

    Se agrega toda la seccion de menu de Jquery y se hacen los primeros 4 ejercicios

--- 09-jQuery/js/app.js
+++ 09-jQuery/js/app.js
@@ -1,76 +1,69 @@
-//add Task
-$(function () {
-
-    //Check LocalStorage
-    if (localStorage.getItem('todolist') != null) {
+$(function() {
+ 
+    // Check localStorage
+    if (localStorage.getItem('todolist') !=  null) {
         $('.list').html(localStorage.getItem('todolist'))
         countTasks()
         countRemains()
     } else {
-        //Count Task & Remains
+        // Count Tasks & Remains
         countTasks()
         countRemains()
     }
-
-    $('footer').on('click', '#add', function () {
-        if ($('#input-task').val().length > 0) {
-
-            $task = ' <article> \
-                <input type="checkbox"> \
-                <p>'+ $('#input-task').val() + '</p> \
-                <button>&times;</button> \
-            </article>'
-            $('section.list').append($task)
+ 
+    // Add Task
+    $('footer').on('click', '#add', function() {
+        if($('#input-task').val().length > 0) {
+ 
+            $task = '<article> \
+                        <input type="checkbox"> \
+                        <p>'+$('#input-task').val()+'</p> \
+                        <button>&times;</button> \
+                     </article>'
+            $('section.list').append($task)         
             $('#input-task').val('')
             countTasks()
             countRemains()
         } else {
-            alert('please! Enter a task!')
+            alert('Please! Enter a Task')
         }
     })
-    //Toggle Task (Remain/Done)
-
-    $('body').on('click', 'input[type=checkbox]', function () {
-        //if checked
+    // Reset Tasks
+    $('body').on('click', '#reset', function() {
+        localStorage.setItem('todolist', $('.list').html(''))
+        countTasks()
+        countRemains()
+    })
+    // Toggle Task (Remain/Done)
+    $('body').on('click', 'input[type=checkbox]', function() {
+        // If checked
         if ($(this).prop('checked')) {
             $(this).attr('checked', true)
             $(this).parent().addClass('checked')
         } else {
-            $(this).attr('checked', true)
+            $(this).attr('checked', false)
             $(this).parent().removeClass('checked')
         }
         countRemains()
     })
-
-    //Remove Task
-    $('body').on('click', 'article button', function () {
+    // Remove Task
+    $('body').on('click', 'article button', function() {
         $(this).closest('article').remove()
         countTasks()
         countRemains()
     })
-
+ 
 })
-//Count Tasks
+// Count Tasks
 function countTasks() {
     $('.num-tasks').text($('article').length)
-    $('title-tasks').text($('article').length > 1 ? 'Tasks' : 'Task')
+    $('.title-tasks').text($('article').length >1?'Tasks':'Task')
 }
-
-//Count Remains
+// Count Remains
 function countRemains() {
     $remain = Math.abs($('.checked').length - $('article').length)
     $('.num-remains').text($remain)
-    $('.title-remains').text($remain > 1 ? 'Remains' : 'Remain')
-
-    //Set LocalStorage
-
+    $('.title-remains').text($remain >1?'Remains':'Remain')
+    // Set localStorage
     localStorage.setItem('todolist', $('.list').html())
 }
-
-// Eliminar todas las tareas
-$('footer').on('click', '#delete-all', function () {
-    $('section.list').empty()
-    countTasks()
-    countRemains()
-    localStorage.setItem('todolist', '')
-})
