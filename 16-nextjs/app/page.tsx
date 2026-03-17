3b727ab3959f18f35bfd39fbef796611960fd2f0
unknown <federico.marin.cardona@gmail.com>
2026-03-17 17:44:33 -0500

    Se crea la carpeta 16-nextjs

---
+++ 16-nextjs/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="bg-indigo-950 min-h-dvh flex flex-col items-center justify-center gap-4 p-4">
+      <h2 className="text-4x1">Hello Next JS</h2>
+      <p className="text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque quo cupiditate veritatis ducimus tenetur itaque nemo suscipit dignissimos quidem saepe possimus, repellat, minima officiis totam ipsum porro. Sapiente, ab officia?</p>
+      <link href="singin" className="" />
+    </div>
+  );
+}
